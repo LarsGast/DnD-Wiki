f69bc15dd7a8641c26a2dff86fc054d9c6ec5e58
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2024-09-18 16:54:58 +0200

    Split up character list making in multiple functions

--- assets/js/character-list.js
+++ assets/js/character-list.js
@@ -69,34 +69,89 @@ const fillList = function(characters) {
 const getCharacterLinkElement = function(character) {
     const listItem = document.createElement('li');
 
+    listItem.appendChild(getCharacterAnchor(character));
+
+    return listItem;
+}
+
+/**
+ * Get the anchor element for a link to a character.
+ * @param {object} character 
+ * @returns 
+ */
+const getCharacterAnchor = function(character) {
+
     // To make it clickable.
     const link = document.createElement('a');
     link.href = `../characters/${character.name.replace(/\s+/g, '-').toLowerCase()}`;
-    listItem.appendChild(link);
+
+    link.appendChild(getCharacterSummaryWrapper(character));
+
+    return link;
+}
+
+/**
+ * Get the summary div element for a link to a character.
+ * @param {object} character 
+ * @returns 
+ */
+const getCharacterSummaryWrapper = function(character) {
 
     // To bundle elements
     const summaryWrapper = document.createElement('div');
     summaryWrapper.className = 'character-summary-wrapper';
-    link.appendChild(summaryWrapper);
+
+    summaryWrapper.appendChild(getCharacterHeader(character));
+    summaryWrapper.appendChild(getCharacterIconWrapper(character));
+
+    return summaryWrapper;
+}
+
+/**
+ * Get the header element for a link to a character.
+ * @param {object} character 
+ * @returns 
+ */
+const getCharacterHeader = function(character) {
 
     // Name of the character.
     const header = document.createElement('h2');
     header.textContent = character.name;
-    summaryWrapper.appendChild(header);
+
+    return header;
+}
+
+/**
+ * Get the icon wrapper element for a link to a character.
+ * @param {object} character 
+ * @returns 
+ */
+const getCharacterIconWrapper = function(character) {
 
     // To bundle all the icons.
     const icons = document.createElement('div');
     icons.className = 'character-icons';
-    summaryWrapper.appendChild(icons);
 
     // All the associated icons.
     character.icons.forEach(function(icon) {
-        const iconSpan = document.createElement('span');
-        iconSpan.classList.add('icon');
-        iconSpan.classList.add(icon.name);
-        iconSpan.title = window.icons.filter(i => i.name === icon.name)[0].description;
-        icons.appendChild(iconSpan);
+        icons.appendChild(getCharacterIconSpan(icon));
     });
 
-    return listItem;
+    return icons;
+}
+
+/**
+ * Get the icon span element for a link to a character.
+ * @param {object} character 
+ * @returns 
+ */
+const getCharacterIconSpan = function(icon) {
+    
+    const iconSpan = document.createElement('span');
+
+    iconSpan.classList.add('icon');
+    iconSpan.classList.add(icon.name);
+    iconSpan.title = window.icons.filter(i => i.name === icon.name)[0].description;
+
+    return iconSpan;
 }
