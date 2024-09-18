c814d81bdf99a141b232575242673c5126eee0b5
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2024-09-18 18:37:46 +0200

    Move code to a different file

--- assets/js/character-list.js
+++ assets/js/character-list.js
@@ -1,50 +1,15 @@
 import { getCharacterLinkElement } from './character-list-element.js';
+import { getRelatedCharacters } from './related-characters.js';
 
 document.addEventListener("DOMContentLoaded", function() {
-
-    // Load in the name of the object of the page that we're currently on.
-    const currentObjectName = window.currentObject;
 
     // Get all the related characters to this object.
     // This includes all information about the character as the characters.yml file specifies.
     // Unsorted.
-    const relatedCharacters = getRelatedCharacters(currentObjectName);
+    const relatedCharacters = getRelatedCharacters();
 
     fillList(relatedCharacters);
 });
-
-/**
- * Get all related characters to the given character
- * @param {string} characterName Name of the character who we're currently viewing
- * @returns All characters that are related to the given one, as specified in _data/characters.yml
- */
-const getRelatedCharacters = function(characterName) {
-    const characterLinks = window.characterLinks;
-
-    // Get all links to this character.
-    const relatedCharacterLinks = characterLinks.filter(characterLink => 
-        characterLink.firstCharacterName === characterName ||
-        characterLink.secondCharacterName === characterName);
-    
-    // Get only the names, so we can use these to get full character objects.
-    // Be sure to filter out the name of the character itself.
-    let relatedCharacterNames = relatedCharacterLinks.map(characterLink => characterLink.firstCharacterName)
-        .concat(relatedCharacterLinks.map(characterLink => characterLink.secondCharacterName))
-        .filter(name => name !== characterName);
-    
-    // Remove dupes.
-    relatedCharacterNames = [...new Set(relatedCharacterNames)];    
-    
-    // Get the actual character objects.
-    // Remove invisible characters, unless we want to see them (showInvisible: true in _config.yml).
-    const charactersData = window.charactersData;
-    const relatedCharacters = charactersData.filter(character => 
-        relatedCharacterNames.includes(character.name) &&
-        (character.visible != false || window.showInvisible)
-    );
-
-    return relatedCharacters;
-}
 
 /**
  * Fill the list that's displayed on the page with all the related characters.
---
+++ assets/js/related-characters.js
@@ -0,0 +1,66 @@
+/**
+ * Get all related characters to the given character
+ * @param {string} objectName Name of the character who we're currently viewing
+ * @returns All characters that are related to the given one, as specified in _data/characters.yml
+ */
+export const getRelatedCharacters = function() {  
+
+    const relatedCharacterNames = getRelatedCharacterNames();
+    
+    // Get the actual character objects.
+    // Remove invisible characters, unless we want to see them (showInvisible: true in _config.yml).
+    const charactersData = window.charactersData;
+    const relatedCharacters = charactersData.filter(character => 
+        relatedCharacterNames.includes(character.name) &&
+        (character.visible != false || window.showInvisible)
+    );
+
+    return relatedCharacters;
+}
+
+const getRelatedCharacterNames = function() {
+    const currentObjectType = window.currentObjectType;
+    const currentObjectName = window.currentObjectName;
+
+    switch (currentObjectType) {
+        case "character":
+            return getCharacterCharacterLinks(currentObjectName);
+        case "location":
+            return getLocationCharacterLinks(currentObjectName);
+    }
+}
+
+const getLocationCharacterLinks = function(locationName) {
+
+    const links = window.locationCharacterLinks;
+
+    // Get all links to this location.
+    const relatedLinks = links.filter(link => link.locationName === locationName);
+
+    // Get only the names, so we can use these to get full character objects.
+    // Remove dupes.
+    const relatedCharacterNames = relatedLinks.map(link => link.characterName);
+
+    return relatedCharacterNames;
+}
+
+const getCharacterCharacterLinks = function(characterName) {
+
+    const characterLinks = window.characterCharacterLinks;
+
+    // Get all links to this character.
+    const relatedCharacterLinks = characterLinks.filter(characterLink => 
+        characterLink.firstCharacterName === characterName ||
+        characterLink.secondCharacterName === characterName);
+    
+    // Get only the names, so we can use these to get full character objects.
+    // Be sure to filter out the name of the character itself.
+    let relatedCharacterNames = relatedCharacterLinks.map(characterLink => characterLink.firstCharacterName)
+        .concat(relatedCharacterLinks.map(characterLink => characterLink.secondCharacterName))
+        .filter(name => name !== characterName);
+    
+    // Remove dupes.
+    relatedCharacterNames = [...new Set(relatedCharacterNames)];
+
+    return relatedCharacterNames;
+}
