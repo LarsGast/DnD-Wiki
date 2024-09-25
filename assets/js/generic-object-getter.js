0a9c9793bd2dc6f9a4c4588249f12cafa953a7d8
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2024-09-25 15:31:34 +0200

    Support same-object list

--- assets/js/generic-object-getter.js
+++ assets/js/generic-object-getter.js
@@ -13,6 +13,16 @@ export const getObjects = function() {
 }
 
 const getObjectNames = function() {
+
+    if (window.layout === window.listType) {
+        return getObjectNamesForSameAsLayout();
+    }
+
+    return getObjectNamesForDifferentAsLayout();
+}
+
+const getObjectNamesForDifferentAsLayout = function() {
+
     const links = window.links;
 
     // Get all location links to this object.
@@ -20,4 +30,26 @@ const getObjectNames = function() {
 
     // Get only the names, so we can use these to get full location objects.
     return relatedLinks.map(link => link[`${window.listType}Name`]);
+}
+
+const getObjectNamesForSameAsLayout = function() {
+
+    const links = window.links;
+    const layoutCapitalized = window.layout.charAt(0).toUpperCase() + window.layout.slice(1);
+
+    // Get all location links to this object.
+    const relatedLinks = links.filter(link => 
+        link[`first${layoutCapitalized}Name`] === window.currentObjectName || 
+        link[`second${layoutCapitalized}Name`] === window.currentObjectName
+    );
+
+    // Get only the names, so we can use these to get full location objects.
+    let relatedObjectNames = relatedLinks.map(link => link[`first${layoutCapitalized}Name`])
+        .concat(relatedLinks.map(link => link[`second${layoutCapitalized}Name`]))
+        .filter(name => name !== window.currentObjectName);
+    
+    // Remove dupes.
+    relatedObjectNames = [...new Set(relatedObjectNames)];
+
+    return relatedObjectNames;
 }
