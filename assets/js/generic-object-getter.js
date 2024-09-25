7fccdfed27e2cb377d4b5c5002b6de7bf1d9ef7d
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2024-09-25 15:36:27 +0200

    Add  comments

--- assets/js/generic-object-getter.js
+++ assets/js/generic-object-getter.js
@@ -1,7 +1,10 @@
-
-
+/**
+ * Get all the related objects as specified in the .yml files based on the page layout, current object, and type of list.
+ * @returns {object[]}
+ */
 export const getObjects = function() {
 
+    // Get the names of the related objects.
     const objectNames = getObjectNames();   
 
     // Get the actual related objects.
@@ -12,6 +15,11 @@ export const getObjects = function() {
     return relatedObjects;
 }
 
+/**
+ * Get the names of all the related objects based on the page layout, current object, and type of list.
+ * Split between lists that are the same type as the page and lists that are a different type as the page.
+ * @returns {string[]}
+ */
 const getObjectNames = function() {
 
     if (window.layout === window.listType) {
@@ -21,6 +29,11 @@ const getObjectNames = function() {
     return getObjectNamesForDifferentAsLayout();
 }
 
+/**
+ * Get the names of all the related objects that use a list type that is not the same as the page layout.
+ * So no character list on a character's page or location list on a location's page.
+ * @returns {string[]}
+ */
 const getObjectNamesForDifferentAsLayout = function() {
 
     const links = window.links;
@@ -32,18 +45,25 @@ const getObjectNamesForDifferentAsLayout = function() {
     return relatedLinks.map(link => link[`${window.listType}Name`]);
 }
 
+/**
+ * Get the names of all the related objects that use a list type that is the same as the page layout.
+ * So a character list on a character's page or a location list on a location's page.
+ * @returns {string[]}
+ */
 const getObjectNamesForSameAsLayout = function() {
 
     const links = window.links;
+
+    // The name of the property used to get the link is lowerCamelCase. 
     const layoutCapitalized = window.layout.charAt(0).toUpperCase() + window.layout.slice(1);
 
-    // Get all location links to this object.
+    // Get all object links to this object.
     const relatedLinks = links.filter(link => 
         link[`first${layoutCapitalized}Name`] === window.currentObjectName || 
         link[`second${layoutCapitalized}Name`] === window.currentObjectName
     );
 
-    // Get only the names, so we can use these to get full location objects.
+    // Get only the names, so we can use these to get full objects.
     let relatedObjectNames = relatedLinks.map(link => link[`first${layoutCapitalized}Name`])
         .concat(relatedLinks.map(link => link[`second${layoutCapitalized}Name`]))
         .filter(name => name !== window.currentObjectName);
