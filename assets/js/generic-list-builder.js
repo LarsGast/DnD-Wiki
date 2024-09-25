6dc8e1d613c7e0b0c2352abdebae9bff966d1bc1
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2024-09-25 17:16:35 +0200

    Add comments and rename

--- assets/js/generic-list-builder.js
+++ assets/js/generic-list-builder.js
@@ -1,6 +1,13 @@
 import { getObjectListElements } from './generic-object-list-wrapper-elements.js';
 
-export const myFunction = function(objects, links, listType, containerId) {
+/**
+ * Fills the given div (by ID) with the required elements based on various input parameters.
+ * @param {object[]} objects All objects of the listType, as specified in the various .yml files
+ * @param {object[]} links All links between the listType and the page layout
+ * @param {string} listType The type of list that is being created
+ * @param {string} containerId The ID of the container div in which the elements should be placed
+ */
+export const fillObjectListDiv = function(objects, links, listType, containerId) {
     window.allObjects = objects;
     window.links = links;
     window.listType = listType;
