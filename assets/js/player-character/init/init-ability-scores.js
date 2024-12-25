f14e81ecea56742e3fa0ffdab7e8be2a4391e16b
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2024-12-25 14:57:16 +0100

    Split functionality into different files

---
+++ assets/js/player-character/init/init-ability-scores.js
@@ -0,0 +1,50 @@
+import { getPlayerCharacterProperty } from "../../local-storage-util.js";
+import { changeAbilityScore, getAbilityScoreModifier } from "../util.js";
+
+/**
+ * Initialize all elements for the ability scores on the PC builder page.
+ */
+export const initAbilityScores = function() {
+    const abilityScoresList = document.getElementById('ability-scores-list'); 
+    const abilityScoreItems = Array.from(abilityScoresList.children);
+    abilityScoreItems.forEach(abilityScoreItem => {
+        initAbilityScoreItem(abilityScoreItem);
+    });
+}
+
+/**
+ * Initialize a single li ability score element.
+ * @param {HTMLLIElement} abilityScoreItem 
+ */
+const initAbilityScoreItem = function(abilityScoreItem) {
+
+    const abilityName = abilityScoreItem.id;
+
+    initAbilityScoreInputField(abilityName);
+    initAbilityScoreModifierSpan(abilityName);
+}
+
+/**
+ * Initialize the input field of the given ability.
+ * @param {string} abilityName 
+ */
+const initAbilityScoreInputField = function(abilityName) {
+
+    const inputField = document.getElementById(`${abilityName}_i`);
+
+    inputField.value = getPlayerCharacterProperty(abilityName);
+    inputField.onchange = function() {
+        changeAbilityScore(abilityName, this.value);
+    };
+}
+
+/**
+ * Initialize the ability score modifier span.
+ * @param {string} abilityName 
+ */
+const initAbilityScoreModifierSpan = function(abilityName) {
+
+    const span = document.getElementById(`${abilityName}_m`);
+
+    span.textContent = getAbilityScoreModifier(abilityName);
+}
---
+++ assets/js/player-character/init/init-skills.js
@@ -0,0 +1,66 @@
+import { isProficientInSkill, isExpertInSkill, changeProficiency, changeExpertise, getSkillModifier } from "../util.js";
+
+/**
+ * Initialize all elements for the skills on the PC builder page.
+ */
+export const initSkills = function() {
+    const skillsList = document.getElementById('skills-list');
+    const skillsListItems = Array.from(skillsList.children);
+    skillsListItems.forEach(skillListItem => {
+        initSkillListItem(skillListItem);
+    });
+}
+
+/**
+ * Initialize a single li skill element.
+ * @param {HTMLLIElement} skillListItem 
+ */
+const initSkillListItem = function(skillListItem) {
+
+    const skill = window.skills.filter(skill => skill.name === skillListItem.id)[0];
+
+    initProficiencyCheckbox(skill);
+    initExpertiseCheckbox(skill);
+    initSkillModifierSpan(skill);
+}
+
+/**
+ * Initialize the proficiency checkbox of a skill item.
+ * @param {object} skill 
+ */
+const initProficiencyCheckbox = function(skill) {
+
+    const proficiencyCheckbox = document.getElementById(`${skill.name}_p`);
+
+    proficiencyCheckbox.checked = isProficientInSkill(skill.name);
+    proficiencyCheckbox.disabled = isExpertInSkill(skill.name);
+    proficiencyCheckbox.onchange = function () {
+        changeProficiency(skill, this.checked);
+    };
+}
+
+/**
+ * Initialize the expertise checkbox of a skill item.
+ * @param {object} skill 
+ */
+const initExpertiseCheckbox = function(skill) {
+
+    const expertiseCheckbox = document.getElementById(`${skill.name}_e`);
+    
+    expertiseCheckbox.checked = isExpertInSkill(skill.name);
+    expertiseCheckbox.disabled = !isProficientInSkill(skill.name);
+    expertiseCheckbox.onchange = function () {
+        changeExpertise(skill, this.checked);
+    };
+}
+
+/**
+ * Initialize the modifier span of a skill item.
+ * @param {object} skill 
+ */
+const initSkillModifierSpan = function(skill) {
+
+    const skillModifierSpan = document.getElementById(`${skill.name}_m`);
+
+    skillModifierSpan.textContent = getSkillModifier(skill);
+}
