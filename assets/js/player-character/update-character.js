b593b276677aaab7949f41b10aa163a8b9d31bda
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-02-19 17:25:57 +0100

    Add cleanPlayerCharacter() to ensure the PC always has the needed properties for the PC-builder page

--- assets/js/player-character/update-character.js
+++ assets/js/player-character/update-character.js
@@ -1,3 +1,4 @@
+import { getDefaultPlayerCharacter } from "../local-storage-util.js";
 import { update_version_2 } from "./update/update-version-2.js";
 
 /**
@@ -11,5 +12,31 @@ export const updateCharacter = function(playerCharacter) {
     if (!playerCharacter.version || playerCharacter.version < 2) {
         update_version_2(playerCharacter);
         playerCharacter.version = 2;
+    }
+
+    // Always clean.
+    cleanPlayerCharacter(playerCharacter);
+}
+
+/**
+ * Clean the PC JSON so it EXACTLY has the properties needed for the PC-Builder page.
+ * This is always based on the current version of the PC blueprint.
+ * @param {JSON} playerCharacter 
+ */
+const cleanPlayerCharacter = function(playerCharacter) {
+    const defaultCharacter = getDefaultPlayerCharacter();
+
+    // Remove unwanted keys
+    for (const key in playerCharacter) {
+        if (!defaultCharacter.hasOwnProperty(key)) {
+            delete playerCharacter[key];
+        }
+    }
+
+    // Add missing keys with default values
+    for (const key in defaultCharacter) {
+        if (!playerCharacter.hasOwnProperty(key)) {
+            playerCharacter[key] = defaultCharacter[key];
+        }
     }
 }
