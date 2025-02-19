e5ac7d18f2689efa3f345b45c4d8db7fdb0e356a
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-02-19 19:56:09 +0100

    Pc api enhancements (#76)
    * Add /api to the base URL for API calls to the SRD
    
    * Add generic function that calls the SRD API
    
    * Use generic function instead of getEquipmentObjectAsync()
    
    * Support EquipmentCategoryIndex in API
    
    * Use getApiResultsAsync() for inventory
    
    * Use getApiResultsAsync() for classes
    
    * Use getApiResultsAsync() for races
    
    * Use getApiResultsAsync() for backgrounds
    
    * Use getApiResultsAsync() for alignments
    
    * Use getApiResultsAsync() for equipment proficiencies
    
    * Refactor build-inventory.js
    
    * Rename skills.js and functions
    
    * Build skills using API
    
    * Init skills using API
    
    * Remove local skills file
    
    * Rename ability scores to reflect the SRD API
    
    * Fix ability score modifier for skills
    
    * Add a cache for the API calls
    
    * Fix bug with inventory modifiers
    
    * Fix bug load martial ranged weapons
    
    * Add update script to update old PC versions to the up to date ones
    
    * use getPlayerCharacterProperty instead of JSON props
    
    * Revert "use getPlayerCharacterProperty instead of JSON props"
    
    This reverts commit c390ea96173d542346aeb8d2c4559459647c9f44.
    
    * Add failsafe for null values to kebak-case
    
    * Use update versions instead of date for updating character
    
    * Add test cases
    
    * Fix update pc to version 2 unset properties
    
    * Add load-page.js and move some logic for updating PCs around
    
    * Fix comment
    
    * Add test cases for 2.0 PCs
    
    * Add cleanPlayerCharacter() to ensure the PC always has the needed properties for the PC-builder page
    
    * Extra gaurd clauses to only update PCs that need updating

--- assets/js/local-storage-util.js
+++ assets/js/local-storage-util.js
@@ -85,18 +85,19 @@ export const getDefaultPlayerCharacter = function() {
         race: null,
         background: null,
         alignment: null,
-        strength: 10,
-        dexterity: 10,
-        constitution: 10,
-        intelligence: 10,
-        wisdom: 10,
-        charisma: 10,
+        str: 10,
+        dex: 10,
+        con: 10,
+        int: 10,
+        wis: 10,
+        cha: 10,
         proficiencies: [],
         expertises: [],
         weapon_proficiencies: [],
         armor_proficiencies: [],
         inventory_weapons: [],
         inventory_armor: [],
-        notes: null
+        notes: null,
+        version: 2
     };
 }
