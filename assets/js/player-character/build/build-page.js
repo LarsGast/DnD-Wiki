400d7417a7d914cf0eae26468b9de79aa230d3b7
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-04-02 20:27:03 +0200

    Remove unused code

--- assets/js/player-character/build/build-page.js
+++ assets/js/player-character/build/build-page.js
@@ -1,4 +1,3 @@
-import { fillGenericInfoElements } from "./generic-info.js";
 import { buildSkills } from "./build-skills.js";
 import { buildEquipmentProficiencies } from "./build-equipment-proficiencies.js";
 
@@ -7,7 +6,6 @@ import { buildEquipmentProficiencies } from "./build-equipment-proficiencies.js"
  * This means filling all elements with necessary data and such.
  */
 export const buildPage = async function() {
-    await fillGenericInfoElements();
     await buildSkills();
     await buildEquipmentProficiencies();
 }
--- assets/js/player-character/init/init-main-properties.js
+++ assets/js/player-character/init/init-main-properties.js
@@ -1,8 +1,5 @@
-import { initAlignment } from "./main-properties/init-alignment.js";
-import { initBackground } from "./main-properties/init-background.js";
 import { initClassAndLevel } from "./main-properties/init-class.js";
 import { initName } from "./main-properties/init-name.js";
-import { initRace, initSubRace } from "./main-properties/init-race.js";
 
 /**
  * Initialize all elements for the main properties on the PC builder page.
@@ -10,8 +7,4 @@ import { initRace, initSubRace } from "./main-properties/init-race.js";
 export const initMainProperties = async function() {
     initName();
     initClassAndLevel();
-    await initRace();
-    await initSubRace();
-    initBackground();
-    initAlignment();
 }
