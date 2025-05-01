01a39b041ca55fe2db8a43bb9e58859c3d23a289
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-05-01 15:27:04 +0200

    First draft PlayerCharacterBank.js

---
+++ assets/js/player-character/objects/PlayerCharacterBank.js
@@ -0,0 +1,135 @@
+import { PlayerCharacter } from "./PlayerCharacter.js";
+
+/**
+ * Key used for saving and loading the player character from localStorage.
+ * @constant {string}
+ */
+const PLAYER_CHARACTER_BANK_KEY = "playerCharacterBank";
+
+export class PlayerCharacterBank {
+
+    /**
+     * @type {PlayerCharacterBankEntry[]}
+     */
+    playerCharacterBankEntries = [];
+
+    /**
+     * Constructs a new PlayerCharacterBank instance.
+     * If data is provided, properties are assigned from it.
+     * @param {JSON} data Optional initial data for the character bank.
+     */
+    constructor(data = {}) {
+        Object.assign(this, data);
+    }
+
+    static load() {
+        try {
+            const playerCharactersAsString = localStorage.getItem(PLAYER_CHARACTER_BANK_KEY);
+
+            // If no stored PC is found, create a new default one.
+            // Should only occur if the user visits the site for the very first time.
+            if (!playerCharactersAsString) {
+                const defaultBank = new PlayerCharacterBank();
+                defaultBank.addNewCharacter(PlayerCharacter.getDefault());
+                return defaultBank;
+            }
+
+            const playerCharactersAsJson = JSON.parse(playerCharactersAsString);
+
+            return new PlayerCharacterBank(playerCharactersAsJson);
+        }
+        catch (error) {
+            console.error("Error parsing player character bank JSON:", error);
+            return new PlayerCharacterBank();
+        }
+    }
+
+    save() {
+        try {
+            localStorage.setItem(PLAYER_CHARACTER_BANK_KEY, JSON.stringify(this));
+        } catch (error) {
+            console.error("Error while saving player character bank:", error);
+        }
+    }
+
+    addNewCharacter(playerCharacter) {
+        const bankEntry = new PlayerCharacterBankEntry();
+
+        bankEntry.playerCharacter = playerCharacter;
+
+        this.playerCharacterBankEntries.push(bankEntry);
+
+        this.save();
+    }
+
+    saveExistingCharacter(id, playerCharacter) {
+        const existingCharacter = this.getCharacterById(id);
+
+        if (!existingCharacter) {
+            throw new Error(`No character found with given ID: ${id}`);
+        }
+
+        existingCharacter.playerCharacter = playerCharacter;
+        existingCharacter.lastEdited = new Date();
+
+        this.save();
+    }
+
+    getCharacterById(id) {
+        return this.playerCharacterBankEntries.find(entry => entry.id === id);
+    }
+
+    /**
+     * @type {PlayerCharacterBankEntry}
+     */
+    getActivePlayerCharacter() {
+        return this.playerCharacterBankEntries.find(entry => entry.isActive);
+    }
+    
+    /**
+     * @type {PlayerCharacterBankEntry[]}
+     */
+    getInactivePlayerCharacters() {
+        return this.playerCharacterBankEntries.filter(entry => !entry.isActive);
+    }
+}
+
+/**
+ * Global singleton instance containing all PC information.
+ * Loaded from localStorage; if no saved data exists, defaults are provided.
+ * @type {PlayerCharacterBank}
+ */
+export const globalPlayerCharacterBank = PlayerCharacterBank.load();
+
+class PlayerCharacterBankEntry {
+
+    /**
+     * UUID.
+     * @type {string}
+     */
+    id = self.crypto.randomUUID();;
+
+    /**
+     * @type {boolean}
+     */
+    isActive = false;
+
+    /**
+     * @type {PlayerCharacter}
+     */
+    playerCharacter;
+
+    /**
+     * @type {Date}
+     */
+    lastEdited = new Date();
+
+    /**
+     * Constructs a new PlayerCharacterBankEntry instance.
+     * If data is provided, properties are assigned from it.
+     * @param {JSON} data Optional initial data for the character bank entry.
+     */
+    constructor(data = {}) {
+        Object.assign(this, data);
+    }
+}
