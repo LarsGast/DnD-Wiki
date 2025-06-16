20fee77038b62b93f13968d7f8c7095e01a6ddd1
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2025-06-16 20:43:55 +0200

    Use the custom object type select when creating new custom objects

--- assets/js/player-character/objects/custom-elements/custom-objects/NewCustomObjectButton.js
+++ assets/js/player-character/objects/custom-elements/custom-objects/NewCustomObjectButton.js
@@ -1,6 +1,5 @@
 import { globals } from "../../../load-page.js";
 import { ApiObjectInfo } from "../../api/resources/ApiObjectInfo.js";
-import { CustomObjectBank } from "../../CustomObjectBank.js"
 
 export class NewCustomObjectButton extends HTMLButtonElement {
     
@@ -8,18 +7,47 @@ export class NewCustomObjectButton extends HTMLButtonElement {
         super();
         
         // Set type and display text.
+        this.disabled = true;
         this.type = 'button';
         this.textContent = "New";
 
         // Bind click event to add a default object to the bank.
         this.onclick = () => this.handleClick();
     }
+
+    /**
+     * Called when the element is connected to the DOM.
+     * Listens for the "customElementTypeChanged" event to show the dialog.
+     */
+    connectedCallback() {
+        this._updateHandler = (event) => this.updateButtonData(event);
+        document.addEventListener("customElementTypeChanged", this._updateHandler);
+    }
+    
+    /**
+     * Called when the element is disconnected from the DOM.
+     * Removes the event listener.
+     */
+    disconnectedCallback() {
+        document.removeEventListener("customElementTypeChanged", this._updateHandler);
+    }
+
+    /**
+     * 
+     * @param {CustomEvent} event 
+     */
+    updateButtonData(event) {
+        /** @type {string} */
+        this.apiCategoryName = event.detail.apiCategoryName;
+
+        this.disabled = false;
+    }
     
     /**
      * Handles the button click.
      */
     handleClick() {
-        globals.customObjectBank.addNewCustomObject(ApiObjectInfo.getDefault());
+        globals.customObjectBank.addNewCustomObject(ApiObjectInfo.getDefault(), this.apiCategoryName);
         globals.customObjectBank.save();
 
         document.dispatchEvent(new Event("newCustomObjectCreated"));
